2ec3ac8050151870968066933815d844904e1205
hossain-sanowar <md.sanowar21@gmail.com>
2026-03-25 22:31:27 +0100

    add llama model

---
+++ medical_Chatbot_LLMs(Llama)-LangChain-Pinecone-Flask-AWS/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name= "medical-chatbot",
+    version= "0.1.0",
+    author= "Md Sanowar Hossain",
+    author_email="md.sanowar21@gmail.com",
+    packages= find_packages(),
+    intstall_requires=[]
+)
